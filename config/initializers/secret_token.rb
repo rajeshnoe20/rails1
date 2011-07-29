51e8f04f02f6e43d99f6f12993446a0f1d3ac873
rajeshrs <rajeshnoe20@gmail.com>
2011-07-29 09:51:41 +0530

    rails app push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railssample::Application.config.secret_token = '8c1f86d9397a98c38f99225f255df8f0cde7ab79a5b804976ca1781e1c513d76ee5efed9a1ef474ec660ceeff8d47cd04d515813819b9d233211028ff8782e42'
